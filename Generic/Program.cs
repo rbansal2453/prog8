21c411354ac61868343fda7ed0689779ddecd75a
Rajesh <Rajesh@RAJESH-PC>
2017-02-20 23:51:58 -0600

    Add project files.

---
+++ Generic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 10;
+            b = 20;
+          
+            Console.WriteLine("Int values before calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Swap<int>(ref a, ref b);
+            Console.WriteLine("Int values after calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.ReadKey();
+        }
+    }
+}
